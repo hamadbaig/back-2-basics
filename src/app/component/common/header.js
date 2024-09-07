1be97a755ca9150468853623a7f829cbd85d0224
hamadbaig <hammad.ahmed.connect@gmail.com>
2024-09-08 02:42:13 +0500

    ui updated error resolved

--- src/app/component/common/header.js
+++ src/app/component/common/header.js
@@ -49,6 +49,12 @@ const Header = () => {
               About Us
             </Link>
             <Link
+              href="/Services"
+              className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+            >
+              Services
+            </Link>
+            <Link
               href="/Contact"
               className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
             >
